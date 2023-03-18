6ea4ce05e7f755029380c308a29d8c2453019498
Samet Yıldırım <sametyldrym2@gmail.com>
2023-03-18 23:34:32 +0300

    Create word2vec.py

---
+++ word2vec.py
@@ -0,0 +1,27 @@
+import numpy as np
+from gensim.models import Word2Vec
+from sklearn.monifold import TSNE
+import matplotlib.pyplot as plt
+
+# prepare data
+f = open('dataset.txt', 'r', encoding='utf8')
+text = f.read()
+t_list = text.split('\n')
+
+
+corpus = []
+
+# put sentences in list
+for sentence in t_list:
+corus.append(cumle.split())
+
+#print to screen
+print(corpus[:10])
+
+
+#train model
+model = Word2Vec(corpus, size=100, window=5, min_count=5, sg=1)
+
+#test model
+model.wv['Ankara']
+model.wv.most_smilar('Turkiye')
